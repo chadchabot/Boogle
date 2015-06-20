7c39ea3d06c55c76d9935fdc196c3c4d65582dc3
Chad Chabot <chadchabot@gmail.com>
2015-06-20 00:31:43 -0400

    Why am I revisiting this 3 years later?
    It was poorly thought out way back when (last pushed in 2011!), and I had a moment
    of shame when looking at the state of this public repo.
    As I am bored, or on the phone, or instead of doing innumerable other wastes of time
    perhaps I'll continue working on this, and do something about the stupid and
    terrible tkinter stuff I tacked on to this for some unknown reason.

--- boogle.py
+++ boogle.py
@@ -3,77 +3,85 @@ import random
 
 
 def boogle():
-    # list of letters on each cube
-    cube1 = ['A', 'A', 'C', 'I', 'O', 'T']
-    cube2 = ['D', 'E', 'N', 'O', 'S', 'W']
-    cube3 = ['A', 'B', 'I', 'L', 'T', 'Y']
-    cube4 = ['D', 'K', 'N', 'O', 'T', 'U']
-    cube5 = ['A', 'B', 'J', 'M', 'O', 'Qu']
-    cube6 = ['E', 'E', 'F', 'H', 'I', 'Y']
-    cube7 = ['A', 'C', 'D', 'E', 'M', 'P']
-    cube8 = ['E', 'G', 'I', 'N', 'T', 'V']
-    cube9 = ['A', 'C', 'E', 'L', 'S', 'R']
-    cube10 = ['E', 'G', 'K', 'L', 'U', 'Y']
-    cube11 = ['A', 'D', 'E', 'N', 'V', 'Z']
-    cube12 = ['E', 'H', 'I', 'N', 'P', 'S']
-    cube13 = ['A', 'H', 'M', 'O', 'R', 'S']
-    cube14 = ['E', 'L', 'P', 'S', 'T', 'U']
-    cube15 = ['B', 'F', 'I', 'O', 'R', 'X']
-    cube16 = ['G', 'I', 'L', 'R', 'U', 'W']
+    # letter distribution matches official game
+
+    cube1 = Cube(['A', 'A', 'C', 'I', 'O', 'T'])
+    cube2 = Cube(['D', 'E', 'N', 'O', 'S', 'W'])
+    cube3 = Cube(['A', 'B', 'I', 'L', 'T', 'Y'])
+    cube4 = Cube(['D', 'K', 'N', 'O', 'T', 'U'])
+    cube5 = Cube(['A', 'B', 'J', 'M', 'O', 'Qu'])
+    cube6 = Cube(['E', 'E', 'F', 'H', 'I', 'Y'])
+    cube7 = Cube(['A', 'C', 'D', 'E', 'M', 'P'])
+    cube8 = Cube(['E', 'G', 'I', 'N', 'T', 'V'])
+    cube9 = Cube(['A', 'C', 'E', 'L', 'S', 'R'])
+    cube10 = Cube(['E', 'G', 'K', 'L', 'U', 'Y'])
+    cube11 = Cube(['A', 'D', 'E', 'N', 'V', 'Z'])
+    cube12 = Cube(['E', 'H', 'I', 'N', 'P', 'S'])
+    cube13 = Cube(['A', 'H', 'M', 'O', 'R', 'S'])
+    cube14 = Cube(['E', 'L', 'P', 'S', 'T', 'U'])
+    cube15 = Cube(['B', 'F', 'I', 'O', 'R', 'X'])
+    cube16 = Cube(['G', 'I', 'L', 'R', 'U', 'W'])
     
     # list of available cubes
-    master_cube_list = [cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16]
-    generate_board(master_cube_list)
+    master_cube_list = [cube1, cube2, cube3, cube4,
+                        cube5, cube6, cube7, cube8,
+                        cube9, cube10, cube11, cube12,
+                        cube13, cube14, cube15, cube16]
 
-
+    cube_list = generate_board(master_cube_list)
+    print_board(cube_list)
 
 def generate_board(master_cube_list):
-    current_cube_list = master_cube_list
-    
-    # have a list of all available cubes
-    # as cubes are removed, pop that cube from the master list
-    
-    remaining_cube_count = 16
-    cubes_picked = 0
-    row = ''
-    letter = ''
-    last_letter = ''
-    print '\r'
-    for x in range(0, 16):
-        # number of remaining cubes
-        cubes_picked += 1
-        y = random.randint(0,remaining_cube_count-1)
-    #    print str(remaining_cube_count) + ' cubes available \t ' + str(y) + ' chosen'
-    #    print 'list item '+str(y) + '\t ' + str(remaining_cube_count) + ' cubes remaining'
-    
-        # use randint() to choose one of the cube faces
-        cube_face = random.randint(0,5)
-        letter = current_cube_list[y-1][cube_face]
-    #    print current_cube_list[y-1][cube_face]
-        if len(row) == 0:
-            row = letter
-        else:
-            if last_letter == 'Qu':
-                row = row + ' ' + letter
-            else:
-                row = row + '  ' + letter
-    
-        last_letter = letter
-        if cubes_picked % 4 == 0:
-            # print out the row of letters that were chosen
-            print '\t' + row
-            row = ''
-    #        print '\r'
-    #    print current_cube_list[y-1]
-    #    print current_cube_list
-        current_cube_list.pop(y-1)
-        remaining_cube_count = remaining_cube_count -1
-    
-    print '\r'
-    
+  return scramble_board(master_cube_list)
+
+# take the 16 cubes and scramble their order
+def scramble_board(cube_list):
+  cube_list = cube_list
+  scrambled_board = []
+  remaining_cube_count = num_cubes = len(cube_list)
+  cube_count = 0
+  for x in range(0, num_cubes):
+    next_cube_num = random.randint(0, remaining_cube_count-1)
+    cube_count += 1
+    next_cube = cube_list.pop(next_cube_num)
+    scrambled_board.append(next_cube)
+    remaining_cube_count-=1
+
+  return scrambled_board
+
+# print board to stdout
+def print_board(board):
+  row = ''
+  cubes_picked = 0
+  print '\r'
+  for cube in board:
+    cubes_picked += 1
+    if len(row) == 0:
+      row = cube.face()
+    else:
+      row = row + cube.face()
+
+    if cubes_picked % 4 == 0:
+      cubes_picked = 0
+      print '\t' + row
+      row = ''
+  print '\r'
 
 def main():
     boogle()
 
+class Cube(object):
+  def __init__(self, faces):
+    self.faces = faces
+    self.chosen_face = random.randint(0,5)
+
+  def face(self):
+    letter = self.faces[self.chosen_face]
+    if letter == 'Qu':
+      return 'Qu '
+    else:
+      return letter + '  '
+
 if __name__ == '__main__':
   main()
+
