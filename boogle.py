e3c89b206cd5488925e63213acbf93fbe52c0c63
Chad Chabot <chadchabot@gmail.com>
2015-06-20 00:56:44 -0400

    A bit more cleanup

--- boogle.py
+++ boogle.py
@@ -3,47 +3,44 @@ import random
 
 
 def boogle():
-    # letter distribution matches official game
+  cube_list = generate_board()
+  print_board(cube_list)
 
-    cube1 = Cube(['A', 'A', 'C', 'I', 'O', 'T'])
-    cube2 = Cube(['D', 'E', 'N', 'O', 'S', 'W'])
-    cube3 = Cube(['A', 'B', 'I', 'L', 'T', 'Y'])
-    cube4 = Cube(['D', 'K', 'N', 'O', 'T', 'U'])
-    cube5 = Cube(['A', 'B', 'J', 'M', 'O', 'Qu'])
-    cube6 = Cube(['E', 'E', 'F', 'H', 'I', 'Y'])
-    cube7 = Cube(['A', 'C', 'D', 'E', 'M', 'P'])
-    cube8 = Cube(['E', 'G', 'I', 'N', 'T', 'V'])
-    cube9 = Cube(['A', 'C', 'E', 'L', 'S', 'R'])
-    cube10 = Cube(['E', 'G', 'K', 'L', 'U', 'Y'])
-    cube11 = Cube(['A', 'D', 'E', 'N', 'V', 'Z'])
-    cube12 = Cube(['E', 'H', 'I', 'N', 'P', 'S'])
-    cube13 = Cube(['A', 'H', 'M', 'O', 'R', 'S'])
-    cube14 = Cube(['E', 'L', 'P', 'S', 'T', 'U'])
-    cube15 = Cube(['B', 'F', 'I', 'O', 'R', 'X'])
-    cube16 = Cube(['G', 'I', 'L', 'R', 'U', 'W'])
-    
-    # list of available cubes
-    master_cube_list = [cube1, cube2, cube3, cube4,
-                        cube5, cube6, cube7, cube8,
-                        cube9, cube10, cube11, cube12,
-                        cube13, cube14, cube15, cube16]
+def generate_board():
+  # letter distribution matches official game
+  cube1 = Cube(['A', 'A', 'C', 'I', 'O', 'T'])
+  cube2 = Cube(['D', 'E', 'N', 'O', 'S', 'W'])
+  cube3 = Cube(['A', 'B', 'I', 'L', 'T', 'Y'])
+  cube4 = Cube(['D', 'K', 'N', 'O', 'T', 'U'])
+  cube5 = Cube(['A', 'B', 'J', 'M', 'O', 'Qu'])
+  cube6 = Cube(['E', 'E', 'F', 'H', 'I', 'Y'])
+  cube7 = Cube(['A', 'C', 'D', 'E', 'M', 'P'])
+  cube8 = Cube(['E', 'G', 'I', 'N', 'T', 'V'])
+  cube9 = Cube(['A', 'C', 'E', 'L', 'S', 'R'])
+  cube10 = Cube(['E', 'G', 'K', 'L', 'U', 'Y'])
+  cube11 = Cube(['A', 'D', 'E', 'N', 'V', 'Z'])
+  cube12 = Cube(['E', 'H', 'I', 'N', 'P', 'S'])
+  cube13 = Cube(['A', 'H', 'M', 'O', 'R', 'S'])
+  cube14 = Cube(['E', 'L', 'P', 'S', 'T', 'U'])
+  cube15 = Cube(['B', 'F', 'I', 'O', 'R', 'X'])
+  cube16 = Cube(['G', 'I', 'L', 'R', 'U', 'W'])
 
-    cube_list = generate_board(master_cube_list)
-    print_board(cube_list)
+  # list of available cubes
+  master_cube_list = [cube1, cube2, cube3, cube4,
+                      cube5, cube6, cube7, cube8,
+                      cube9, cube10, cube11, cube12,
+                      cube13, cube14, cube15, cube16]
 
-def generate_board(master_cube_list):
   return scramble_board(master_cube_list)
 
 # take the 16 cubes and scramble their order
 def scramble_board(cube_list):
-  cube_list = cube_list
+  cube_list = cube_list #arg is a list (mutable) so we have to create ref in scope
   scrambled_board = []
   remaining_cube_count = num_cubes = len(cube_list)
-  cube_count = 0
   for x in range(0, num_cubes):
-    next_cube_num = random.randint(0, remaining_cube_count-1)
-    cube_count += 1
-    next_cube = cube_list.pop(next_cube_num)
+    next_cube_idx = random.randint(0, remaining_cube_count-1)
+    next_cube = cube_list.pop(next_cube_idx)
     scrambled_board.append(next_cube)
     remaining_cube_count-=1
 
